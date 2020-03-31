a5dc8860de5992461344ecbbaa9ae45c2c45f492
unknown <mrg119@ic.ac.uk>
2020-03-31 23:06:27 +0300

    responsive inventory slots

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -7,7 +7,10 @@ public class Inventory : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        for (int i = 0; i < 10; i++)
+        {
+            Debug.Log("Hello " + i);
+        }
     }
 
     // Update is called once per frame
@@ -15,4 +18,11 @@ public class Inventory : MonoBehaviour
     {
         
     }
+
+    void OnInvSlot()
+    {
+        
+    }
+
+    
 }
