dc20f3500d296902f46474e42c2762e68e6676db
unknown <mrg119@ic.ac.uk>
2020-04-03 01:09:52 +0300

    prefabs stored

---
+++ Assets/Scripts/InventoryHandlers/InventoryPrefabs.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryPrefabs : MonoBehaviour
+{
+    public static InventoryPrefabs instance;
+    public GameObject invContentPrefab;
+    public GameObject weaponSlotPrefab;
+    public GameObject otherSlotPrefab;
+
+    public static GameObject InvContentPrefab
+    {
+        get => instance.invContentPrefab;
+    }
+
+    public static GameObject WeaponSlotPrefab
+    {
+        get => instance.weaponSlotPrefab;
+    }
+
+    public GameObject OtherSlotPrefab
+    {
+        get => instance.otherSlotPrefab;
+    }
+
+    void Awake()
+    {
+        instance = this;
+    }
+}
