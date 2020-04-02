81ead097b018c4ad64b1448e4fd1fd722152d4a5
unknown <mrg119@ic.ac.uk>
2020-04-02 20:25:34 +0300

    weapon category choice works

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,6 +6,14 @@ public class GameManager : MonoBehaviour
 {
     // Start is called before the first frame update
     public const string WEAPON_ICONS_PATH = "Game/Icons/Weapon/";
+    public const string MELEE_NAME = "Melee";
+    public const string RIFLE_NAME = "Rifle";
+    public const string SHOTGUN_NAME = "Shotgun";
+    public const string SNIPER_NAME = "Sniper";
+    public const string MINIGUN_NAME = "Minigun";
+    public const string BAZOOKA_NAME = "Bazooka";
+    public const string GRENADE_NAME = "Grenade";
+    public static Color TRANSPARENT = new Color(0, 0,0, 0);
     void Start()
     {
         
